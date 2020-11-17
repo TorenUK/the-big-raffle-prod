628c333247b0a210838441d970f9efc571f061ca
TorenUK <td_fraser@hotmail.co.uk>
2020-11-17 17:27:09 +0000

    add banner logo // profile login

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //components
 import "./Banner.css";
 
+// other
+import logo from "../images/logo-br.svg";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
+import { Button } from "@material-ui/core";
+
 const Banner = () => {
+  const [user, setUser] = useState("td_fraser@hotmail.co.uk");
+
   return (
     <div className="banner">
-      <h1>the big raffle</h1>
+      <div className="banner__left">
+        <img src={logo} />
+        <h1>THE BIG RAFFLE</h1>
+      </div>
+      <div className="banner__right">
+        <div className="banner__right__user">
+          <PersonOutlineOutlinedIcon />
+          <p>{user}</p>
+        </div>
+        <div className="banner__right__logout">
+          <Button variant="outlined">logout</Button>
+        </div>
+      </div>
     </div>
   );
 };
