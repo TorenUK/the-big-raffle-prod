7a75007a0880ef4c86f9c121e439772523625979
TorenUK <td_fraser@hotmail.co.uk>
2020-11-21 15:02:51 +0000

    add order slice

--- frontend/src/components/Order.js
+++ frontend/src/components/Order.js
@@ -12,30 +12,29 @@ import axios from "./axios";
 // redux
 import { useDispatch, useSelector } from "react-redux";
 import { selectUser } from "../features/user/userSlice";
-import { emptyCart, selectCart } from "../features/cart/cartSlice";
+import { selectOrder } from "../features/order/orderSlice";
 
 const Order = () => {
   const user = useSelector(selectUser);
-  const cart = useSelector(selectCart);
+  const order = useSelector(selectOrder);
   const dispatch = useDispatch();
-
-  const [order, setOrder] = useState([]);
-
-  useEffect(() => {
-    const getOrder = async () => {
-      const response = await axios.get(`/order/find/?email=${user}`);
-
-      setOrder(response.data);
-      console.log(order, "the order");
-    };
-  }, []);
 
   return (
     <div className="order">
       <Banner />
       <div className="order__container">
-        <h1>thanks for your order!</h1>
-        <h3>{user}</h3>
+        <div className="order__top">
+          <h1>thanks for your order!</h1>
+          <h3>{user}</h3>
+        </div>
+        <div className="order__id">
+          <h2>{order.name}</h2>
+          <h3>order number: {order.order}</h3>
+        </div>
+        <div className="order__address">
+          <h3>{order.address.line1}</h3>
+          <h3>{order.address.postcode}</h3>
+        </div>
       </div>
       <Footer />
     </div>
